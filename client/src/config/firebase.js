7cd81dca531160709d6af328e9227b982ec25f7c
habenzy <rstauss@burlingtoncodeacademy.com>
2023-12-03 11:16:30 -0500

    vite did some dumb shit

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -3,8 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import dotenv from "dotenv";
-dotenv.config();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,13 +10,13 @@ dotenv.config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STOREAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.REACT_APP_FIREBASE_STOREAGE_BUCKET,
+  messagingSenderId: import.meta.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
