13420895cb96a0c2959beaf606e12bf784f296c2
habenzy <rstauss@burlingtoncodeacademy.com>
2023-12-03 11:42:23 -0500

    Im an idiot

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -10,16 +10,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.VITE_FIREBASE_STOREAGE_BUCKET,
-  messagingSenderId: import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STOREAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
-console.log(import.meta.VITE_FIREBASE_API_KEY)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
