78e71e4026bd3c540da44b91f61ed6867cfc7f40
habenzy <rstauss@burlingtoncodeacademy.com>
2024-04-15 10:33:44 -0400

    shrink the burger

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STOREAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBt1XPQKSrYdLn_NFPMLankYL-D785Mk1Y",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "phantom-reboot.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "phantom-reboot",
+  storageBucket: import.meta.env.VITE_FIREBASE_STOREAGE_BUCKET || "phantom-reboot.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:764738129006:web:a752f4159e22125b87aa7f",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "764738129006"
 };
 
 // Initialize Firebase
