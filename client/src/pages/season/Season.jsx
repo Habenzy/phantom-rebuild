56a5529df051aeb700a41628dab929fa60812db5
habenzy <rstauss@burlingtoncodeacademy.com>
2024-05-12 09:58:16 -0400

    updated season year

--- client/src/pages/season/Season.jsx
+++ client/src/pages/season/Season.jsx
@@ -146,7 +146,7 @@ function Season(props) {
 
   return (
     <div className="season_container">
-      <h1>Season 2023</h1>
+      <h1>Season 2024</h1>
       {allShows
         ? allShows.map((show) => {
             return (
