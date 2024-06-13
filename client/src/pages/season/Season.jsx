6e511ed4943a938c248e8caf7a5493bdc0e19ef0
habenzy <rstauss@burlingtoncodeacademy.com>
2024-06-13 10:12:43 -0400

    better logging

--- client/src/pages/season/Season.jsx
+++ client/src/pages/season/Season.jsx
@@ -119,8 +119,9 @@ function Season(props) {
         if(location.hash) {
           
           setTimeout(() => {
-            document.getElementById(location.hash.slice(1)).scrollIntoView()
             console.log("scrolling to " + location.hash.slice(1))
+            document.getElementById(location.hash.slice(1)).scrollIntoView()
+            
             //window.scroll({top: location.hash, behavior: "smooth"})
           }, 2000)
           
